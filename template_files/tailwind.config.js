8cc0a94a735749b68bea2621bb5165f659827e4a
Paul Rouge <paulrouge@outlook.com>
2023-02-26 18:32:21 +0100

    added updated tailwind config to template_files dir

--- template_files/tailwind.config.js
+++ template_files/tailwind.config.js
@@ -19,4 +19,32 @@ module.exports = {
     },
   },
   plugins: [],
+};/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}", 
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          customBlue: '#0000FF',
+          customMint: '#80FFCB',
+          customPink: '#F400E9',
+          funPurple: '#67399B',
+          funRed: '#E33B40',
+          funOrange: ' #F8712E',
+          funYellow: '#F9DB0E',
+          funBlue: '#48B7CD',
+        },
+        fontFamily: {
+          customFont: ['Poppins', 'sans-serif'],
+        },
+        zIndex: {
+          '60': 300,
+        },
+      },
+    },
+    plugins: [],
 };
