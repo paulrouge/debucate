3100a27c1894323de83d876d6c9e43e48084c244
PaulRouge <65511035+paulrouge@users.noreply.github.com>
2023-03-06 12:43:17 +0100

    Create contract_functions.ts

---
+++ utils/constants/contract_functions.ts
@@ -0,0 +1,12 @@
+export enum LotteryFunctions {
+    name = 'name',
+    getLotteryIdCounter = 'getLotteryIdCounter',
+    createNewLottery = 'createNewLottery',
+    buyTickets = 'buyTickets',
+    closeLottery = 'closeLottery',
+    getLottery = 'getLottery',
+    getOwnerLotteryTicket = 'getOwnerLotteryTicket',
+    getLotteryWinner = 'getLotteryWinner',
+    getLotteryPayout = 'getLotteryPayout', 
+    getLotteryPrizePool = 'getLotteryPrizePool',
+}
