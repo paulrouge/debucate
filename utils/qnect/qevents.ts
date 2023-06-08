19f496fc1bb4a83629514f00ba420242ad8c0ac5
Paul Rouge <paulrouge@outlook.com>
2023-06-08 17:29:26 +0200

    clean up

--- utils/qnect/qevents.ts
+++
@@ -1,6 +0,0 @@
-// set the tx-events that are used in the project
-// export enum qEvents {
-//     TicketsBought = 'qNect_tickets_bought',
-//     CreatedNewLottery = 'qNect_created_new_lottery',
-//     PickedWinner = 'qNect_picked_winner',
-// }
