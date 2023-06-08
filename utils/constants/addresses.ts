f1865e995ea41beb0e4a967d44158b2e19d1c478
Paul Rouge <paulrouge@outlook.com>
2023-06-08 21:20:00 +0200

    new nft contract

--- utils/constants/addresses.ts
+++ utils/constants/addresses.ts
@@ -10,7 +10,8 @@ export type ContractAddresses = {
 export const ContractAddresses:ContractAddresses = {
     // BSC Testnet
     97:{
-        NFT_contract: '0x14270E64449B736b54781de99d11F655fb81f16D', 
+        // NFT_contract: '0x14270E64449B736b54781de99d11F655fb81f16D', 
+        NFT_contract: '0x1be47c787cBaE190C6779e0d0931b0F9C0F09894', 
         BUSD_contract: '0xe46eC5a68381dc7edE298d62D02f9403edfd2136' 
     },
     // Sepolia ETH2 Testnet
