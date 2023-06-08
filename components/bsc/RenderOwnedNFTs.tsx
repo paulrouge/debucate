c8b90ac3b17eab1edaf82a95ed1a7aa7022a493c
Paul Rouge <paulrouge@outlook.com>
2023-06-08 13:56:24 +0200

    render nft cards in render container

--- components/bsc/RenderOwnedNFTs.tsx
+++ components/bsc/RenderOwnedNFTs.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import useBSCDapp from '@/hooks/useBSCDapp'
-
+import NFTCard from './NFTCard'
 
 const RenderOwnedNFTs = () => {
-    const { contractReader } = useBSCDapp()
-  
-    return (
-    <div>RenderOwnedNFTs</div>
+  const { nftsOwned } = useBSCDapp()
+
+  return (
+    <div className='flex flex-col gap-4 bg-pinkFun mt-6'>
+      <div className='flex flex-col gap-4'>
+        {
+          nftsOwned.map((nft, index) => {
+            return <NFTCard key={index} id={nft} />;
+          })
+        }
+      </div>
+    </div>
   )
 }
 
