adb96d0ffdbaf555ed67f812bd7d2986701f1e45
Paul Rouge <paulrouge@outlook.com>
2023-06-09 08:07:44 +0200

    added comments to contract functions enum type

--- utils/constants/contract_functions.ts
+++ utils/constants/contract_functions.ts
@@ -1,3 +1,9 @@
+/*
+    This file contains the contract functions that are used in the frontend.
+    This is done to avoid typos and to have a single source of truth for the contract functions.
+*/
+
+// Lottery contract functions as an example on how to use this file.
 export enum LotteryFunctions {
     name = 'name',
     getLotteryIdCounter = 'getLotteryIdCounter',
