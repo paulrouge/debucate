67112eb08cd6923ec87550d161d406ca94a1a158
Paul Rouge <paulrouge@outlook.com>
2023-06-09 08:05:06 +0200

    added color change on connected with wallet

--- components/qnect/QNectButton.tsx
+++ components/qnect/QNectButton.tsx
@@ -4,19 +4,19 @@ import { motion } from "framer-motion"
 
 
 export default function QNectButton() {
-    const { setConnectModalOpen } = useGlobalContext();
+    const { setConnectModalOpen, account } = useGlobalContext();
   
     return (
     <motion.div 
     whileHover={{ scale: 1.05 }}
     whileTap={{ scale: 0.9 }}
     transition={{ duration: 0.04 }}
-    className='
-    border border-gray-300 z-40
+    className={`border border-gray-300 z-40
     fixed top-4 right-4 font-bold px-2 py-1
-    bg-blue-500 text-white flex justify-center 
+    text-white flex justify-center 
     items-center rounded-xl cursor-pointer
-    '
+    ${account ? 'bg-green-500' : 'bg-funBlue'}
+    `}
     onClick={()=>setConnectModalOpen(true)}
     >
         Connect
