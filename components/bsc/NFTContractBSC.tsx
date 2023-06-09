03b544f4832ec43ac6d5b8988b27ede9af260ad3
Paul Rouge <paulrouge@outlook.com>
2023-06-09 08:42:13 +0200

    added conditional on dashboard for connecting wallet

--- components/bsc/NFTContractBSC.tsx
+++ components/bsc/NFTContractBSC.tsx
@@ -1,21 +1,27 @@
-
 import React from 'react'
 import Mint from './Mint'
 import RenderOwnedNFTs from './RenderOwnedNFTs'
+import { useGlobalContext } from '@/utils/context/globalContext'
 
 const DappContractBSC = () => {
+    const { account } = useGlobalContext()
 
     return (
     <div
     className='flex flex-col items-center justify-center px-6 py-2 bg-funOrange rounded-lg font-mono'>
         <div className='text-2xl py-4 font-bold'>NFT Stuff!</div>
         <div className='flex flex-col gap-2'>
-            <div>
-                <Mint/>
-                <RenderOwnedNFTs />
-            </div>
+            { account === '' ?
+                <div className='text-xl font-bold'>
+                    Connect your wallet to start!
+                </div>
+                :
+                <div>
+                    <Mint/>
+                    <RenderOwnedNFTs />
+                </div>
+            }
         </div>
-
     </div>
   )
 }
